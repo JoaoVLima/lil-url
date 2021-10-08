623fc95aef0f623728b914c23f9eb9718aae8504
joaovlima <celu.joaolima@gmail.com>
2021-10-07 22:42:21 -0300

    Adding Urls

---
+++ core/urls.py
@@ -0,0 +1,23 @@
+"""short_url URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, re_path
+
+from .views import IndexView, ShortView
+
+urlpatterns = [
+    re_path(r'^$', IndexView.as_view(), name='index'),
+    re_path(r'^(?P<short>[a-zA-Z0-9]+)$', ShortView.as_view(), name='short'),
+]
